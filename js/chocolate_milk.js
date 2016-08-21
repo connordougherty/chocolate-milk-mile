c0e51ec9569244c4138a544d871c684834b57dd8
Connor Dougherty <connordougherty01@gmail.com>
2016-08-20 20:21:04 -0400

    Removed unnecessary files

--- js/chocolate_milk.js
+++ js/chocolate_milk.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   $("#pour").delay(800).slideDown("slow", function() {
   	$("#milk").animate({height: '250px'}, 1600, function() {
   		$("#pour").slideUp("slow", function() {
-  			$("#squirt").slideDown(800, function() {
-  				$("#squirt").slideUp(700, function() {
+  			$("#syrup").slideDown(800, function() {
+  				$("#syrup").slideUp(700, function() {
   					$("#straw").animate({left: '100px'}, function() {
   						$("#straw").animate({left: '40px'}, function() {
 	  						$("#straw").animate({left: '100px'}, function() {
